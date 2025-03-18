26f946bc5954f69e48507845b0e61b3eedd1f8c2
fernandoroj3345 <39432486+fernandoroj3345@users.noreply.github.com>
2025-03-18 18:05:50 -0300

    Se modifico factorial.py

--- TP1/src/factorial/factorial.py
+++ TP1/src/factorial/factorial.py
@@ -5,6 +5,7 @@
 #* Dr.P.E.Colla (c) 2022                                                   *
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
+"""
 import sys
 
 def factorial(num): 
@@ -27,4 +28,44 @@ else:
     num = int(sys.argv[1])
 
 print("Factorial", num, "! es", factorial(num))
+"""
+##############################################################################
+import sys
+
+def factorial(num): 
+    if num < 0: 
+        print("Factorial de un número negativo no existe")
+        return 0
+    elif num == 0: 
+        return 1
+    else: 
+        fact = 1
+        while(num > 1): 
+            fact *= num 
+            num -= 1
+        return fact 
+
+#Función para validar y procesar el rango
+def procesar_rango(rango):
+    try:
+        inicio, fin = map(int, rango.split('-'))
+        return inicio, fin
+    except ValueError:
+        print("Formato incorrecto. Debe ser 'inicio-fin' con números enteros.")
+        sys.exit()
+
+#Verificar si se pasó un argumento desde la línea de comandos
+if len(sys.argv) < 2:
+    # Si no se pasó un número, solicita un rango al usuario
+    rango = input("Debe informar un rango! Ingrese un rango de la forma inicio-fin: ")
+else:
+    # Si se pasó un número como argumento
+    rango = sys.argv[1]
+
+#Procesar el rango
+inicio, fin = procesar_rango(rango)
+
+#Calcular y mostrar el factorial de los extremos
+print(f"Factorial de {inicio} es {factorial(inicio)}")
+print(f"Factorial de {fin} es {factorial(fin)}")
 
