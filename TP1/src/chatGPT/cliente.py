cd1a708c5f1a7df45f1a853624e67a8eab453524
fernandoroj3345 <39432486+fernandoroj3345@users.noreply.github.com>
2025-04-08 20:28:55 -0300

    agrego carpeta chatGPT

---
+++ TP1/src/chatGPT/cliente.py
@@ -0,0 +1,22 @@
+ response = client.chat.completions.create(
+ model="gpt-4o-mini-2024-07-18",
+ response_format={ "type": "json_object" },
+ messages=[
+ {
+ "role": "system",
+ "content": context },
+ {
+ "role": "user",
+ "content" : usertask },
+ {
+ "role": "user",
+ "content": userquery }
+ ],
+ temperature=1,
+ max_tokens=16384,
+ top_p=1,
+ frequency_penalty=0,
+ presence_penalty=0
+ )
+#*--- post process response message to eliminate garbage from the JSON file
+ jsonStr=response.choices[0].message.content
